693ead904e9f3a5c24c5675326df29aa47f98eac
Doomky <james.richet@epita.fr>
2020-12-16 22:01:24 +0100

    [token_parser] refct: split all asserts on tests

--- tests/test_token_parser.py
+++ tests/test_token_parser.py
@@ -10,37 +10,37 @@ from tokens.operators import Addition, Subtraction, Multiplication, Division
 def test_single_addition():
     addition = "1 + 1"
     result: [Token] = TokenParser(addition).parse()
-    assert len(result) == 3 and \
-           isinstance(result[0], Number) and \
-           isinstance(result[1], Addition) and \
-           isinstance(result[2], Number)
+    assert len(result) == 3
+    assert isinstance(result[0], Number)
+    assert isinstance(result[1], Addition)
+    assert isinstance(result[2], Number)
 
 
 def test_single_subtraction():
     subtraction = "1 - 1"
     result: [Token] = TokenParser(subtraction).parse()
-    assert len(result) == 3 and \
-           isinstance(result[0], Number) and\
-           isinstance(result[1], Subtraction) and\
-           isinstance(result[2], Number)
+    assert len(result) == 3
+    assert isinstance(result[0], Number)
+    assert isinstance(result[1], Subtraction)
+    assert isinstance(result[2], Number)
 
 
 def test_single_multiplication():
     multiplication = "1 * 1"
     result: [Token] = TokenParser(multiplication).parse()
-    assert len(result) == 3 and \
-           isinstance(result[0], Number) and \
-           isinstance(result[1], Multiplication) and \
-           isinstance(result[2], Number)
+    assert len(result) == 3
+    assert isinstance(result[0], Number)
+    assert isinstance(result[1], Multiplication)
+    assert isinstance(result[2], Number)
 
 
 def test_single_division():
     division = "1 / 1"
     result: [Token] = TokenParser(division).parse()
-    assert len(result) == 3 and \
-           isinstance(result[0], Number) and \
-           isinstance(result[1], Division) and \
-           isinstance(result[2], Number)
+    assert len(result) == 3
+    assert isinstance(result[0], Number)
+    assert isinstance(result[1], Division)
+    assert isinstance(result[2], Number)
 
 
 # two digits operations
@@ -48,53 +48,53 @@ def test_single_division():
 def test_two_digit_number_addition():
     addition = "12 + 12"
     result: [Token] = TokenParser(addition).parse()
-    assert len(result) == 3 and \
-           isinstance(result[0], Number) and \
-           isinstance(result[1], Addition) and \
-           isinstance(result[2], Number)
+    assert len(result) == 3
+    assert isinstance(result[0], Number)
+    assert isinstance(result[1], Addition)
+    assert isinstance(result[2], Number)
 
 
 def test_two_digit_number_subtraction():
     subtraction = "12 - 12"
     result: [Token] = TokenParser(subtraction).parse()
-    assert len(result) == 3 and \
-           isinstance(result[0], Number) and \
-           isinstance(result[1], Subtraction) and \
-           isinstance(result[2], Number)
+    assert len(result) == 3
+    assert isinstance(result[0], Number)
+    assert isinstance(result[1], Subtraction)
+    assert isinstance(result[2], Number)
 
 
 def test_two_digit_number_multiplication():
     multiplication = "12 * 12"
     result: [Token] = TokenParser(multiplication).parse()
-    assert len(result) == 3 and \
-           isinstance(result[0], Number) and \
-           isinstance(result[1], Multiplication) and \
-           isinstance(result[2], Number)
+    assert len(result) == 3
+    assert isinstance(result[0], Number)
+    assert isinstance(result[1], Multiplication)
+    assert isinstance(result[2], Number)
 
 
 def test_two_digit_number_division():
     division = "12 / 12"
     result: [Token] = TokenParser(division).parse()
-    assert len(result) == 3 and \
-           isinstance(result[0], Number) and \
-           isinstance(result[1], Division) and \
-           isinstance(result[2], Number)
+    assert len(result) == 3
+    assert isinstance(result[0], Number)
+    assert isinstance(result[1], Division)
+    assert isinstance(result[2], Number)
 
 
 def test_long_operation():
     division = "(12 + 1) * 3 / (4 + 1) "
     result: [Token] = TokenParser(division).parse()
-    assert len(result) == 13 and \
-           isinstance(result[0], Bracket) and \
-           isinstance(result[1], Number) and \
-           isinstance(result[2], Addition) and \
-           isinstance(result[3], Number) and \
-           isinstance(result[4], Bracket) and \
-           isinstance(result[5], Multiplication) and \
-           isinstance(result[6], Number) and \
-           isinstance(result[7], Division) and \
-           isinstance(result[8], Bracket) and \
-           isinstance(result[9], Number) and \
-           isinstance(result[10], Addition) and \
-           isinstance(result[11], Number) and \
-           isinstance(result[12], Bracket)
+    assert len(result) == 13
+    assert isinstance(result[0], Bracket)
+    assert isinstance(result[1], Number)
+    assert isinstance(result[2], Addition)
+    assert isinstance(result[3], Number)
+    assert isinstance(result[4], Bracket)
+    assert isinstance(result[5], Multiplication)
+    assert isinstance(result[6], Number)
+    assert isinstance(result[7], Division)
+    assert isinstance(result[8], Bracket)
+    assert isinstance(result[9], Number)
+    assert isinstance(result[10], Addition)
+    assert isinstance(result[11], Number)
+    assert isinstance(result[12], Bracket)
