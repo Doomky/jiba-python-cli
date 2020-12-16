b8039555f47a3ffcf886fde005d9659dedf703bd
Doomky <james.richet@epita.fr>
2020-12-16 16:12:17 +0100

    [token_parser] test : single operation test fixed variable names

--- tests/test_token_parser.py
+++ tests/test_token_parser.py
@@ -8,17 +8,17 @@ def test_single_addition():
     result: [Token] = TokenParser(addition).parse()
     assert isinstance(result[0], Number) and isinstance(result[1], Operator) and (result[2], Number)
 
-def test_single_substraction():
-    addition = "1 - 1"
-    result: [Token] = TokenParser(addition).parse()
+def test_single_subtraction():
+    subtraction = "1 - 1"
+    result: [Token] = TokenParser(subtraction).parse()
     assert isinstance(result[0], Number) and isinstance(result[1], Operator) and (result[2], Number)
 
 def test_single_multiplication():
-    addition = "1 * 1"
-    result: [Token] = TokenParser(addition).parse()
+    multiplication = "1 * 1"
+    result: [Token] = TokenParser(multiplication).parse()
     assert isinstance(result[0], Number) and isinstance(result[1], Operator) and (result[2], Number)
 
 def test_single_division():
-    addition = "1 / 1"
-    result: [Token] = TokenParser(addition).parse()
+    division = "1 / 1"
+    result: [Token] = TokenParser(division).parse()
     assert isinstance(result[0], Number) and isinstance(result[1], Operator) and (result[2], Number)
