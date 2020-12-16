0d08a3b1690ff4ade4013df7f78531d77e61e32a
Doomky <james.richet@epita.fr>
2020-12-16 16:44:24 +0100

    [token_parser] impl : two digits operations test passed

--- tokens/token_parser.py
+++ tokens/token_parser.py
@@ -1,43 +1,46 @@
 from . import Token
 from . import Number
 from . import Operator
-from enum import Enum
 import re
-
-
-class TokenType(int, Enum):
-    number = 0
-    op_add = 1
-    op_sub = 2
-    op_mul = 3
-    op_div = 4
-
 
 class TokenParser:
     def __init__(self, input_str: str):
         self.input_str = input_str
 
     def _parse_number_token(self, i: int, input_str: str, result_tokens: [Token]):
-        result_tokens.append(Number([int(input_str[i])]))
+
+        digits = [int(input_str[i])]
+        i+= 1
+
+        while (i < len(input_str) and '0' <= input_str[i] <= '9'):
+            digits.append(int(input_str[i]))
+            i+=1
+        result_tokens.append(Number(digits))
+        return i
 
     def _parse_operator_token(self, i: int, input_str: str, result_tokens: [Token]):
         result_tokens.append(Operator())
+        i+=1
+        return i
 
     def parse(self) -> [Token]:
         result_tokens: [Token] = []
 
         input_str = self.input_str
-        for i in range(len(input_str)):
+        i = 0
+        while i < len(input_str):
             char_i = input_str[i]
             if '0' <= char_i <= '9':
-                self._parse_number_token(i, input_str, result_tokens)
+                i = self._parse_number_token(i, input_str, result_tokens)
             elif char_i == '+':
-                self._parse_operator_token(i, input_str, result_tokens)
+                i = self._parse_operator_token(i, input_str, result_tokens)
             elif char_i == '-':
-                self._parse_operator_token(i, input_str, result_tokens)
+                i = self._parse_operator_token(i, input_str, result_tokens)
             elif char_i == '*':
-                self._parse_operator_token(i, input_str, result_tokens)
+                i = self._parse_operator_token(i, input_str, result_tokens)
             elif char_i == '/':
-                self._parse_operator_token(i, input_str, result_tokens)
+                i = self._parse_operator_token(i, input_str, result_tokens)
+            elif char_i == ' ':
+                i += 1
 
         return result_tokens
