f116aa7077cb4eefc77d74efe67c67b2d6ef3afe
Doomky <james.richet@epita.fr>
2020-12-16 23:18:47 +0100

    [addition] test : zero sum and double carry over addition tests added and passed

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -8,6 +8,14 @@ import pytest
 # fixtures
 
 @pytest.fixture
+def zero():
+    return Number([0])
+
+@pytest.fixture
+def zero_large():
+    return Number([0,0,0,0,0,0,0,0,0,0,0,0])
+
+@pytest.fixture
 def one():
     return Number([1])
 
@@ -18,6 +26,10 @@ def eight():
 @pytest.fixture
 def ten():
     return Number([0, 1])
+
+@pytest.fixture
+def ninety_nine():
+    return Number([9, 9])
 
 
 @pytest.fixture
@@ -66,3 +78,21 @@ def test_simple_eight_plus_eight(token_queue, eight):
     assert len(result.digits) == 2
     assert result.digits[0] == 6
     assert result.digits[1] == 1
+
+def test_simple_ninety_nine_plus_one(token_queue, ninety_nine, one):
+    token_queue.put(ninety_nine)
+    token_queue.put(one)
+    addition = Addition()
+    result = addition.compute()
+    assert len(result.digits) == 3
+    assert result.digits[0] == 0
+    assert result.digits[1] == 0
+    assert result.digits[2] == 1
+
+def test_simple_zero_plus_zero_large(token_queue, zero, zero_large):
+    token_queue.put(zero)
+    token_queue.put(zero_large)
+    addition = Addition()
+    result = addition.compute()
+    assert len(result.digits) == 1
+    assert result.digits[0] == 0
