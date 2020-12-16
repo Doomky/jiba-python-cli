1404fe03743e2d70528eebb8ff9efc09a3a8684c
Doomky <james.richet@epita.fr>
2020-12-16 17:43:12 +0100

    [token_parser] test : long operation test added

--- tests/test_token_parser.py
+++ tests/test_token_parser.py
@@ -74,3 +74,21 @@ def test_two_digit_number_division():
            isinstance(result[0], Number) and \
            isinstance(result[1], Operator) and \
            isinstance(result[2], Number)
+
+def test_long_operation():
+    division = "(12 + 1) * 3 / (4 + 1) "
+    result: [Token] = TokenParser(division).parse()
+    assert len(result) == 13 and \
+           isinstance(result[0], Operator) and \
+           isinstance(result[1], Number) and \
+           isinstance(result[2], Operator) and \
+           isinstance(result[3], Number) and \
+           isinstance(result[4], Operator) and \
+           isinstance(result[5], Operator) and \
+           isinstance(result[6], Number) and \
+           isinstance(result[7], Operator) and \
+           isinstance(result[8], Operator) and \
+           isinstance(result[9], Number) and \
+           isinstance(result[10], Operator) and \
+           isinstance(result[11], Number) and \
+           isinstance(result[12], Operator)
