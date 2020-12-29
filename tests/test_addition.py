6d9500083cda9cc92ad93d43401966cf375e71a8
Doomky <james.richet@epita.fr>
2020-12-29 14:50:58 +0100

    [addition] test: signed addition tests added (and sign added to number)

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,5 +1,5 @@
 from tokens.token import Token
-from tokens.number import Number
+from tokens.number import Number, Sign
 from tokens.operators import Addition
 from tokens.token_queue import TokenQueue
 import pytest
@@ -14,6 +14,10 @@ def zero():
 @pytest.fixture
 def zero_large():
     return Number([0,0,0,0,0,0,0,0,0,0,0,0])
+
+@pytest.fixture
+def minus_one():
+    return Number([1], Sign.negative)
 
 @pytest.fixture
 def one():
@@ -41,7 +45,6 @@ def token_queue():
     token_queue.clear()
     return token_queue
 
-
 # simple addition
 
 def test_simple_one_plus_one(token_queue, one):
@@ -49,15 +52,25 @@ def test_simple_one_plus_one(token_queue, one):
     token_queue.put(one)
     addition = Addition()
     result = addition.compute()
+    assert result.sign == Sign.positive
     assert len(result.digits) == 1
     assert result.digits[0] == 2
 
+def test_simple_minus_one_plus_minus_one(token_queue, minus_one):
+    token_queue.put(minus_one)
+    token_queue.put(minus_one)
+    addition = Addition()
+    result = addition.compute()
+    assert result.sign == Sign.negative
+    assert len(result.digits) == 1
+    assert result.digits[0] == 2
 
 def test_simple_one_plus_ten(token_queue, one, ten):
     token_queue.put(one)
     token_queue.put(ten)
     addition = Addition()
     result = addition.compute()
+    assert result.sign == Sign.positive
     assert len(result.digits) == 2
     assert result.digits[0] == 1
     assert result.digits[1] == 1
@@ -68,6 +81,7 @@ def test_simple_ten_plus_one(token_queue, one, ten):
     token_queue.put(one)
     addition = Addition()
     result = addition.compute()
+    assert result.sign == Sign.positive
     assert len(result.digits) == 2
     assert result.digits[0] == 1
     assert result.digits[1] == 1
@@ -78,6 +92,7 @@ def test_simple_eight_plus_eight(token_queue, eight):
     token_queue.put(eight)
     addition = Addition()
     result = addition.compute()
+    assert result.sign == Sign.positive
     assert len(result.digits) == 2
     assert result.digits[0] == 6
     assert result.digits[1] == 1
@@ -87,6 +102,19 @@ def test_simple_ninety_nine_plus_one(token_queue, ninety_nine, one):
     token_queue.put(one)
     addition = Addition()
     result = addition.compute()
+    assert result.sign == Sign.positive
+    assert len(result.digits) == 3
+    assert result.digits[0] == 0
+    assert result.digits[1] == 0
+    assert result.digits[2] == 1
+
+
+def test_simple_ninety_nine_plus_one(token_queue, minus_ninety_nine, minus_one):
+    token_queue.put(minus_ninety_nine)
+    token_queue.put(minus_one)
+    addition = Addition()
+    result = addition.compute()
+    assert result.sign == Sign.negative
     assert len(result.digits) == 3
     assert result.digits[0] == 0
     assert result.digits[1] == 0
@@ -97,6 +125,7 @@ def test_simple_zero_plus_zero_large(token_queue, zero, zero_large):
     token_queue.put(zero_large)
     addition = Addition()
     result = addition.compute()
+    assert result.sign == Sign.positive
     assert len(result.digits) == 1
     assert result.digits[0] == 0
 
@@ -105,6 +134,7 @@ def test_simple_nine_nine_plus_one(token_queue, nine_nine, one):
     token_queue.put(one)
     addition = Addition()
     result = addition.compute()
+    assert result.sign == Sign.positive
     assert len(result.digits) == 10
     assert result.digits[0] == 0
     assert result.digits[1] == 0
--- tokens/number.py
+++ tokens/number.py
@@ -1,13 +1,21 @@
 from . import Token
+from enum import Enum
 
+class Sign(int, Enum):
+    positive = 1
+    negative = 2
 
 class Number(Token):
 
-    def __init__(self, digits : [int]):
+    def __init__(self, digits : [int], sign = Sign.positive):
         self.digits = digits.copy()
+        self.sign = sign
 
     def __str__(self):
         return ''.join([str(digit_int) for digit_int in self.digits])
 
     def compute(self):
         return self
+
+    def opposite_inverse(self):
+        Number(self.digits.copy(), Sign.positive if sign is Sign.negative else Sign.negative)
