3e17fc664a69b2d11f55e310a186c6fa7b1494a2
bastien-seira <bastien.seira@epita.fr>
2020-12-29 16:08:47 +0100

    [subtraction] test : two digits subtraction test added

--- tests/test_subtraction.py
+++ tests/test_subtraction.py
@@ -15,12 +15,23 @@ def two():
     return Number([2])
 
 @pytest.fixture
+def ten():
+    return Number([0, 1])
+
+@pytest.fixture
+def hundred():
+    return Number([0, 0, 1])
+
+
+
+@pytest.fixture
 def token_queue():
     token_queue = TokenQueue.get_instance()
     token_queue.clear()
     return token_queue
 
 # simple subtraction
+
 
 def test_simple_two_minus_one(token_queue, one, two):
     token_queue.put(two)
@@ -30,4 +41,15 @@ def test_simple_two_minus_one(token_queue, one, two):
     assert result.sign == Sign.positive
     assert len(result.digits) == 1
     assert result.digits[0] == 1
+
+
+def test_two_digits_subtraction(token_queue, ten):
+    token_queue.put(Number([0, 5]))
+    token_queue.put(ten)
+    addition = Subtraction()
+    result = addition.compute()
+    assert result.sign == Sign.positive
+    assert len(result.digits) == 2
+    assert result.digits[0] == 0
+    assert result.digits[1] == 4
 
