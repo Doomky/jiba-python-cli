4ce185aa1d9482def4e6caa55ef81c5af12b746b
Ingo Braekman <ingo.braekman@epita.fr>
2021-01-09 16:09:53 +0100

    [multiplication] test : Numbers with zeroes multiplication tests passed

--- tests/test_multiplication.py
+++ tests/test_multiplication.py
@@ -62,6 +62,11 @@ def nine_nine():
 
 
 @pytest.fixture
+def large_number():
+    return Number([2, 6, 3, 8, 7, 6])
+
+
+@pytest.fixture
 def token_queue():
     token_queue = TokenQueue.get_instance()
     token_queue.clear()
@@ -109,3 +114,80 @@ def test_eight_times_four(token_queue, eight, four):
     assert len(result.digits) == 2
     assert result.digits[0] == 2
     assert result.digits[1] == 3
+
+
+# Mulitple digits multiplication
+
+def test_ninety_nine_times_large_number(token_queue, ninety_nine, large_number):
+    token_queue.put(ninety_nine)
+    token_queue.put(large_number)
+    multiplication = Multiplication()
+    result = multiplication.compute()
+    assert result.sign == Sign.positive
+    assert len(result.digits) == 8
+    assert result.digits[0] == 8
+    assert result.digits[1] == 3
+    assert result.digits[2] == 8
+    assert result.digits[3] == 7
+    assert result.digits[4] == 5
+    assert result.digits[5] == 1
+    assert result.digits[6] == 7
+    assert result.digits[7] == 6
+
+
+def test_large_number_times_ninety_nine(token_queue, large_number, ninety_nine):
+    token_queue.put(large_number)
+    token_queue.put(ninety_nine)
+    multiplication = Multiplication()
+    result = multiplication.compute()
+    assert result.sign == Sign.positive
+    assert len(result.digits) == 8
+    assert result.digits[0] == 8
+    assert result.digits[1] == 3
+    assert result.digits[2] == 8
+    assert result.digits[3] == 7
+    assert result.digits[4] == 5
+    assert result.digits[5] == 1
+    assert result.digits[6] == 7
+    assert result.digits[7] == 6
+
+
+def test_nine_nine_times_nine_nine(token_queue, nine_nine):
+    token_queue.put(nine_nine)
+    token_queue.put(nine_nine)
+    multiplication = Multiplication()
+    result = multiplication.compute()
+    assert result.sign == Sign.positive
+    assert len(result.digits) == 18
+    assert result.digits[0] == 1
+    assert result.digits[1] == 0
+    assert result.digits[2] == 0
+    assert result.digits[3] == 0
+    assert result.digits[4] == 0
+    assert result.digits[5] == 0
+    assert result.digits[6] == 0
+    assert result.digits[7] == 0
+    assert result.digits[8] == 0
+    assert result.digits[9] == 8
+    assert result.digits[10] == 9
+    assert result.digits[11] == 9
+    assert result.digits[12] == 9
+    assert result.digits[13] == 9
+    assert result.digits[14] == 9
+    assert result.digits[15] == 9
+    assert result.digits[16] == 9
+    assert result.digits[17] == 9
+
+
+# Numbers with zeroes multiplication
+
+def test_ten_times_ten(token_queue, ten):
+    token_queue.put(ten)
+    token_queue.put(ten)
+    multiplication = Multiplication()
+    result = multiplication.compute()
+    assert result.sign == Sign.positive
+    assert len(result.digits) == 3
+    assert result.digits[0] == 0
+    assert result.digits[1] == 0
+    assert result.digits[2] == 1
