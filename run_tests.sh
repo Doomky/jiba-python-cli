9ee4042ea45502a423292ebed4eb79a3ee4bf763
Allan Cantin <allan.cantin@epita.fr>
2021-01-10 14:48:42 +0100

    [Scripts] added install part to run_tests.sh to ensure pytest installation + fixed missing newline at the end of run.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+python -m venv .venv
+
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+  .venv\\Scripts\\activate.bat
+else
+  source .venv/bin/activate
+fi
+
+python -m pip install -r requirements.txt
 pytest ./tests/
+
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+  .venv\\Scripts\\deactivate.bat
+else
+  deactivate
+fi
