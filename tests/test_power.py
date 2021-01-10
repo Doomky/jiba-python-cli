fc77dc3941fbfbede6363fbb0a72b9f6cb666b41
Ingo Braekman <ingo.braekman@epita.fr>
2021-01-10 14:09:49 +0100

    [power] refct : changed from token queue to token stack

--- tests/test_power.py
+++ tests/test_power.py
@@ -1,6 +1,6 @@
 from tokens.number import Number, Sign
 from tokens.operators import Power, NegativePowerError
-from tokens.token_queue import TokenQueue
+from tokens.token_stack import TokenStack
 import pytest
 
 
@@ -70,41 +70,41 @@ def large_number():
 
 
 @pytest.fixture
-def token_queue():
-    token_queue = TokenQueue.get_instance()
-    token_queue.clear()
-    return token_queue
+def token_stack():
+    token_stack = TokenStack.get_instance()
+    token_stack.clear()
+    return token_stack
 
 
 # Single digit power
 
-def test_one_power_one(token_queue, one):
-    token_queue.put(one)
-    token_queue.put(one)
+def test_one_power_one(token_stack, one):
+    token_stack.push(one)
+    token_stack.push(one)
     power = Power()
     result = power.compute()
     assert result == Number([1])
 
 
-def test_one_power_two(token_queue, one, two):
-    token_queue.put(one)
-    token_queue.put(two)
+def test_one_power_two(token_stack, one, two):
+    token_stack.push(one)
+    token_stack.push(two)
     power = Power()
     result = power.compute()
     assert result == Number([1])
 
 
-def test_two_power_four(token_queue, two, four):
-    token_queue.put(two)
-    token_queue.put(four)
+def test_two_power_four(token_stack, two, four):
+    token_stack.push(two)
+    token_stack.push(four)
     power = Power()
     result = power.compute()
     assert result == Number([6, 1])
 
 
-def test_eight_power_four(token_queue, eight, four):
-    token_queue.put(eight)
-    token_queue.put(four)
+def test_eight_power_four(token_stack, eight, four):
+    token_stack.push(eight)
+    token_stack.push(four)
     power = Power()
     result = power.compute()
     assert result == Number([6, 9, 0, 4])
@@ -112,9 +112,9 @@ def test_eight_power_four(token_queue, eight, four):
 
 # Complex power
 
-def test_ninety_nine_power_five(token_queue, ninety_nine, five):
-    token_queue.put(ninety_nine)
-    token_queue.put(five)
+def test_ninety_nine_power_five(token_stack, ninety_nine, five):
+    token_stack.push(ninety_nine)
+    token_stack.push(five)
     power = Power()
     result = power.compute()
     assert result == Number([9, 9, 4, 0, 0, 9, 9, 0, 5, 9])
@@ -122,17 +122,17 @@ def test_ninety_nine_power_five(token_queue, ninety_nine, five):
 
 # Zero power
 
-def test_zero_power_four(token_queue, zero, four):
-    token_queue.put(zero)
-    token_queue.put(four)
+def test_zero_power_four(token_stack, zero, four):
+    token_stack.push(zero)
+    token_stack.push(four)
     power = Power()
     result = power.compute()
     assert result == Number([0])
 
 
-def test_four_power_zero(token_queue, zero, four):
-    token_queue.put(four)
-    token_queue.put(zero)
+def test_four_power_zero(token_stack, zero, four):
+    token_stack.push(four)
+    token_stack.push(zero)
     power = Power()
     result = power.compute()
     assert result == Number([1])
@@ -140,25 +140,25 @@ def test_four_power_zero(token_queue, zero, four):
 
 # Negative numbers power
 
-def test_minus_one_power_four(token_queue, minus_one, four):
-    token_queue.put(minus_one)
-    token_queue.put(four)
+def test_minus_one_power_four(token_stack, minus_one, four):
+    token_stack.push(minus_one)
+    token_stack.push(four)
     power = Power()
     result = power.compute()
     assert result == Number([1])
 
 
-def test_minus_one_power_five(token_queue, minus_one, five):
-    token_queue.put(minus_one)
-    token_queue.put(five)
+def test_minus_one_power_five(token_stack, minus_one, five):
+    token_stack.push(minus_one)
+    token_stack.push(five)
     power = Power()
     result = power.compute()
     assert result == Number([1], Sign.negative)
 
 
-def test_one_power_minus_one(token_queue, one, minus_one):
+def test_one_power_minus_one(token_stack, one, minus_one):
     with pytest.raises(NegativePowerError):
-        token_queue.put(one)
-        token_queue.put(minus_one)
+        token_stack.push(one)
+        token_stack.push(minus_one)
         power = Power()
         power.compute()
