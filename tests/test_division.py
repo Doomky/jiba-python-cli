1af4e84f860fa84f8b4bb88ce04ed3416f794ea3
Ingo Braekman <ingo.braekman@epita.fr>
2021-01-10 01:08:11 +0100

    [division] test : single digit division tests added

---
+++ tests/test_division.py
@@ -0,0 +1,121 @@
+from tokens.number import Number, Sign
+from tokens.operators import Division
+from tokens.token_queue import TokenQueue
+import pytest
+
+
+# fixtures
+
+@pytest.fixture
+def zero():
+    return Number([0])
+
+
+@pytest.fixture
+def zero_large():
+    return Number([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+
+@pytest.fixture
+def minus_one():
+    return Number([1], Sign.negative)
+
+
+@pytest.fixture
+def one():
+    return Number([1])
+
+
+@pytest.fixture
+def two():
+    return Number([2])
+
+
+@pytest.fixture
+def three():
+    return Number([3])
+
+
+@pytest.fixture
+def four():
+    return Number([4])
+
+
+@pytest.fixture
+def eight():
+    return Number([8])
+
+
+@pytest.fixture
+def ten():
+    return Number([0, 1])
+
+
+@pytest.fixture
+def ninety_nine():
+    return Number([9, 9])
+
+
+@pytest.fixture
+def minus_ninety_nine():
+    return Number([9, 9], Sign.negative)
+
+
+@pytest.fixture
+def nine_nine():
+    return Number([9, 9, 9, 9, 9, 9, 9, 9, 9])
+
+
+@pytest.fixture
+def large_number():
+    return Number([2, 6, 3, 8, 7, 6])
+
+
+@pytest.fixture
+def token_queue():
+    token_queue = TokenQueue.get_instance()
+    token_queue.clear()
+    return token_queue
+
+
+# Single digit division
+
+def test_one_divided_by_one(token_queue, one):
+    token_queue.put(one)
+    token_queue.put(one)
+    division = Division()
+    result = division.compute()
+    assert result.sign == Sign.positive
+    assert len(result.digits) == 1
+    assert result.digits[0] == 1
+
+
+def test_one_divided_by_two(token_queue, one, two):
+    token_queue.put(one)
+    token_queue.put(two)
+    division = Division()
+    result = division.compute()
+    assert result.sign == Sign.positive
+    assert len(result.digits) == 1
+    assert result.digits[0] == 0
+
+
+def test_four_divided_by_two(token_queue, four, two):
+    token_queue.put(four)
+    token_queue.put(two)
+    division = Division()
+    result = division.compute()
+    assert result.sign == Sign.positive
+    assert len(result.digits) == 1
+    assert result.digits[0] == 2
+
+
+def test_eight_divided_by_three(token_queue, eight, three):
+    token_queue.put(eight)
+    token_queue.put(three)
+    division = Division()
+    result = division.compute()
+    assert result.sign == Sign.positive
+    assert len(result.digits) == 1
+    assert result.digits[0] == 2
+
